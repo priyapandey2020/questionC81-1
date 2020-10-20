0f85e71b2b527e7187defcca83d65f05f246e614
priyapandey2020 <priya.pandey@whitehatjr.com>
2020-10-20 14:51:35 +0530

    ..

---
+++ screens/SettingScreen.js
@@ -0,0 +1,17 @@
+import React  from 'react';
+import {View,Text} from 'react-native';
+import {Component}  from 'react';
+import {TextComponent} from 'react-native';
+
+export default class SettingScreen extends Component{
+
+  render(){
+    return(
+      <View>
+        <text>
+          Setting Screen
+        </text>
+      </View>
+    )
+  }
+}
